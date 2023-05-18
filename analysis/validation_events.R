1428dd06549dbf8262dd0cd19a92abfae0ddbe95
wjchulme <25637345+wjchulme@users.noreply.github.com>
2023-05-18 14:29:05 +0100

    replace fuzzy match with non-equi match from data.table

--- analysis/validation_events.R
+++ analysis/validation_events.R
@@ -7,10 +7,10 @@
 
 ## Import libraries ----
 library('tidyverse')
-library('lubridate')
 library('here')
 library('glue')
 library('fuzzyjoin')
+library('data.table')
 
 ## Import custom user functions from lib
 source(here("analysis", "lib", "utility.R"))
@@ -33,8 +33,8 @@ import_sus_data <- function(method){
     patient_id,
     admission_number,
     admission_date,
-    "method_sus{method}" := TRUE,
-    "admission_date_sus{method}" := admission_date
+    "admission_date_sus{method}" := admission_date,
+    "method_sus{method}" := TRUE
   )
 
   return(dat)
@@ -45,8 +45,13 @@ admissions_susB  <- import_sus_data("B")
 admissions_susC  <- import_sus_data("C")
 
 admissions_isaric  <- read_rds(here("output", "admissions", "processed_isaric.rds")) %>%
-  transmute(patient_id, admission_number, admission_date, admission_date_isaric=admission_date) %>%
-  mutate(method_isaric=TRUE)
+  transmute(
+    patient_id,
+    admission_number,
+    admission_date,
+    admission_date_isaric=admission_date,
+    method_isaric=TRUE
+  )
 
 # admissions ----
 
@@ -93,7 +98,14 @@ admissions_joined <-
 
 ascertainment <-
   admissions_joined %>%
+  # recover left-join on isaric admissions
   filter(method_isaric) %>%
+  # remove multiple same-day matches from SUS
+  group_by(patient_id, admission_date) %>%
+  mutate(dup_id = row_number()) %>%
+  ungroup() %>%
+  filter(dup_id==1L) %>%
+  # summarise number of SUS admissions that match isaric admissions
   summarise(
     isaric_n = n(),
     susA_n = sum(method_susA),
@@ -119,110 +131,73 @@ remove(admissions_joined)
 
 # all dates of admission via ISARIC per patient up to <end_date>, and each SUS ascertainment method where a match was found for that date +/- 3 days
 
-# the version below hits memory limits, so refectoring below...
-if(TRUE){
-  admissions_joined_fuzzy <-
-    reduce(
-      lst(
-        admissions_isaric %>% filter(admission_number == 1),
-        admissions_susA %>% select(-admission_number),
-        admissions_susB %>% select(-admission_number),
-        admissions_susC %>% select(-admission_number),
-      ),
-      ~{
-        fuzzy_left_join(
-          .x,
-          .y,
-          by = c("patient_id", "admission_date"),
-          match_fun = list(
-            patient_id = `==`,
-            admission_date = function(x,y){x<=y+1 & x>=y-1}
-          )
-        ) %>%
-          rename(patient_id=patient_id.x, admission_date=admission_date.x) %>%
-          select(-patient_id.y, -admission_date.y)
-      },
-    ) %>%
-    mutate(
-      admission_date_diff_susA = admission_date_susA - admission_date_isaric,
-      admission_date_diff_susB = admission_date_susB - admission_date_isaric,
-      admission_date_diff_susC = admission_date_susC - admission_date_isaric
-    ) %>%
-    replace_na(
-      lst(
-        method_isaric=FALSE,
-        method_susA=FALSE,
-        method_susB=FALSE,
-        method_susC=FALSE,
-      )
-    ) %>%
-    filter(
-      admission_date >= start_date,
-      admission_date <= end_date
+
+function_fuzzy_join <- function(mx, my){
+  joined <- fuzzy_left_join(
+    mx,
+    my,
+    by = c("patient_id", "admission_date"),
+    match_fun = list(
+      patient_id = `==`,
+      admission_date = function(x,y){x>=y-1 & x<=y+1}
     )
+  )
+  renamed <- rename(joined, patient_id=patient_id.x, admission_date=admission_date.x)
+  select(renamed, -patient_id.y, -admission_date.y)
 }
 
 
-# potentially lower memory version:
-# function_fuzzy_join <- function(mx, my){
-#   joined <- fuzzy_left_join(
-#     mx,
-#     my,
-#     by = c("patient_id", "admission_date"),
-#     match_fun = list(
-#       patient_id = `==`,
-#       admission_date = function(x,y){x<=y+1 & x>=y-1}
-#     )
-#   )
-#   renamed <- rename(joined, patient_id=patient_id.x, admission_date=admission_date.x)
-#   select(renamed, -patient_id.y, -admission_date.y)
-# }
-#
-# print("susA")
-# admissions_joined_fuzzy <-
-#   admissions_isaric %>% filter(admission_number == 1) %>%
-#   function_fuzzy_join(
-#     admissions_susA
-#   )
-#
-# print("susB")
-# admissions_joined_fuzzy <-
-#   function_fuzzy_join(
-#     admissions_joined_fuzzy,
-#     admissions_susB
-#   )
-#
-# print("susC")
-# admissions_joined_fuzzy <-
-#   function_fuzzy_join(
-#     admissions_joined_fuzzy,
-#     admissions_susC
-#   ) %>%
-#   mutate(
-#     admission_date_diff_susA = admission_date_susA - admission_date_isaric,
-#     admission_date_diff_susB = admission_date_susB - admission_date_isaric,
-#     admission_date_diff_susC = admission_date_susC - admission_date_isaric
-#   ) %>%
-#   replace_na(
-#     lst(
-#       method_isaric=FALSE,
-#       method_susA=FALSE,
-#       method_susB=FALSE,
-#       method_susC=FALSE,
-#     )
-#   ) %>%
-#   filter(
-#     admission_date >= start_date,
-#     admission_date <= end_date
-#   )
+function_nonequi_join <- function(mx, my){
+  joined <- setDT(my)[
+    ,
+    c("admission_date_pre", "admission_date_post") := list(admission_date-1, admission_date+1)
+  ][
+    setDT(mx),
+    on = .(patient_id, admission_date_pre<=admission_date, admission_date_post>=admission_date)
+  ]
+  joined <- select(joined, -admission_date_pre, -admission_date_post) %>% mutate(admission_date=admission_date_isaric)
+  joined
+}
+
+admissions_joined_nonequi <-
+  reduce(
+    lst(
+      admissions_isaric %>% filter(admission_number == 1),
+      admissions_susA %>% select(-admission_number),
+      admissions_susB %>% select(-admission_number),
+      admissions_susC %>% select(-admission_number),
+    ),
+    ~{function_nonequi_join(.x, .y)},
+  ) %>%
+  mutate(
+    admission_date_diff_susA = admission_date_susA - admission_date_isaric,
+    admission_date_diff_susB = admission_date_susB - admission_date_isaric,
+    admission_date_diff_susC = admission_date_susC - admission_date_isaric
+  ) %>%
+  replace_na(
+    lst(
+      method_isaric=FALSE,
+      method_susA=FALSE,
+      method_susB=FALSE,
+      method_susC=FALSE,
+    )
+  ) %>%
+  filter(
+    admission_date >= start_date,
+    admission_date <= end_date
+  )
 
 
 # sensitivity of SUS for picking up admissions reported in ISARIC -------
 
-print("summarise")
-ascertainment_fuzzy <-
-  admissions_joined_fuzzy %>%
-  filter(method_isaric) %>%
+ascertainment_nonequi <-
+  admissions_joined_nonequi %>%
+  # remove multiple matches within period -- ignore thinking about how to select the most appropriate match for now as it doesn't matter in the summary
+  group_by(patient_id, admission_date) %>%
+  mutate(dup_id = row_number()) %>%
+  ungroup() %>%
+  filter(dup_id==1L) %>%
+  # summarise number of SUS admissions that closely match isaric admissions
   summarise(
     isaric_n = n(),
     susA_n = sum(method_susA),
@@ -241,9 +216,9 @@ ascertainment_fuzzy <-
     names_pattern = "(.*)_(.*)"
   )
 
-ascertainment_fuzzy
+ascertainment_nonequi
 
-write_csv(ascertainment_fuzzy, fs::path(output_dir, "ascertainment_fuzzy.csv"))
+write_csv(ascertainment_nonequi, fs::path(output_dir, "ascertainment_nonequi.csv"))
 
 
 
