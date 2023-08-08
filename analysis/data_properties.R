5015789d45742258df63fb3807a86dc11aa7ada0
Millie Green <acagreen17@gmail.com>
2023-08-08 10:40:20 +0100

    amend script for multiple input files

--- analysis/data_properties.R
+++ analysis/data_properties.R
@@ -11,12 +11,14 @@
 # The first is the dataset that is to be summarised
 # The second in the directory where the txt output will be saved
 #
-# Input: output/admissions/isaric_admission1.csv.gz
+# Input:  output/admissions/isaric_admission1.csv.gz
+#         output/admissions/sus_methodA_admission1_cohortextractor.csv.gz
+#         output/admissions/sus_methodA_admission1_ehrQL.csv.gz
 #
-# Output: /output/data_properties/isaric_admission1*.txt
+# Output: output/data_properties/*.txt
 #
 # Author(s): W. Hulme (edited by M. Green)
-# Date last updated: 04/07/2023
+# Date last updated: 08/07/2023
 #
 ################################################################################
 
@@ -34,90 +36,101 @@ source(here("analysis", "lib", "custom_functions.R"))
 ## Import command-line arguments
 args <- commandArgs(trailingOnly=TRUE)
 
+
+# Data properties tables ----
+
 if(length(args)==0){
   # use for interactive testing
-  csv_file <- "output/admissions/isaric_admission1.csv.gz"
+  csv_files <- list("output/admissions/isaric_admission1.csv.gz",
+                    "output/admissions/sus_methodA_admission1_cohortextractor.csv.gz",
+                    "output/admissions/sus_methodA_admission1_ehrQL.csv.gz")
   output_dir <- "output/data_properties"
 } else {
-  csv_file <- args[[1]]
-  output_dir <- args[[2]]
+  csv_files <- list(args[[1]], args[[2]], args[[3]])
+  output_dir <- args[[4]]
 }
 
-stopifnot("must pass an .csv file" = fs::path_ext(csv_file)=="gz")
+for (i in 1:length(csv_files)){
 
-filenamebase <- fs::path_ext_remove(fs::path_file(csv_file))
+  ## Specify data
+  csv_file <- csv_files[[i]]
 
-## Import processed data
-data <- readr::read_csv(here(csv_file))
+  stopifnot("must pass an .gz file" = fs::path_ext(csv_file)=="gz")
 
-# Output summary .txt
-options(width=200) # set output width for capture.output
+  filenamebase <- fs::path_ext_remove(fs::path_file(csv_file))
 
-dir.create(here(output_dir), showWarnings = FALSE, recursive=TRUE)
+  ## Import data
+  data <- readr::read_csv(here(csv_file))
 
+  # Output summary .txt
+  options(width=200) # set output width for capture.output
 
-## High-level variable overview ----
-capture.output(
-  skimr::skim_without_charts(data),
-  file = here(output_dir, paste0(filenamebase, "_skim", ".txt")),
-  split=FALSE
-)
-
-## list of column types ----
-capture.output(
-  lapply(data, class),
-  file = here(output_dir, paste0(filenamebase, "_coltypes", ".txt"))
-)
+  dir.create(here(output_dir), showWarnings = FALSE, recursive=TRUE)
 
 
-## tabulated data ----
+  ## High-level variable overview ----
+  capture.output(
+    skimr::skim_without_charts(data),
+    file = here(output_dir, paste0(filenamebase, "_skim", ".txt")),
+    split=FALSE
+  )
 
-# delete file if it exists
-if(file.exists(here(output_dir, paste0(filenamebase, "_tabulate", ".txt")))){
-  file.remove(here(output_dir, paste0(filenamebase, "_tabulate", ".txt")))
+  ## list of column types ----
+  capture.output(
+    lapply(data, class),
+    file = here(output_dir, paste0(filenamebase, "_coltypes", ".txt"))
+  )
+
+
+  ## tabulated data ----
+
+  # delete file if it exists
+  if(file.exists(here(output_dir, paste0(filenamebase, "_tabulate", ".txt")))){
+    file.remove(here(output_dir, paste0(filenamebase, "_tabulate", ".txt")))
+  }
+
+
+  ### categorical and logical ----
+  sumtabs_cat <-
+    data %>%
+    select(-ends_with("_id")) %>%
+    select(where(is.character), where(is.logical), where(is.factor)) %>%
+    map(redacted_summary_cat) %>%
+    enframe()
+
+  capture.output(
+    walk2(sumtabs_cat$value, sumtabs_cat$name, print_cat),
+    file = here(output_dir, paste0(filenamebase, "_tabulate", ".txt")),
+    append=FALSE
+  )
+
+
+  ### numeric ----
+  sumtabs_num <-
+    data %>%
+    select(-ends_with("_id")) %>%
+    select(where(~ {!is.logical(.x) & is.numeric(.x) & !is.Date(.x)})) %>%
+    map(redacted_summary_num) %>%
+    enframe()
+
+  capture.output(
+    walk2(sumtabs_num$value, sumtabs_num$name, print_num),
+    file = here(output_dir, paste0(filenamebase, "_tabulate", ".txt")),
+    append=TRUE
+  )
+
+  ### dates ----
+
+  sumtabs_date <-
+    data %>%
+    select(-ends_with("_id")) %>%
+    select(where(is.Date)) %>%
+    map(redacted_summary_date) %>%
+    enframe()
+
+  capture.output(
+    walk2(sumtabs_date$value, sumtabs_date$name, print_num),
+    file = here(output_dir, paste0(filenamebase, "_tabulate", ".txt")),
+    append=TRUE
+  )
 }
-
-
-### categorical and logical ----
-sumtabs_cat <-
-  data %>%
-  select(-ends_with("_id")) %>%
-  select(where(is.character), where(is.logical), where(is.factor)) %>%
-  map(redacted_summary_cat) %>%
-  enframe()
-
-capture.output(
-  walk2(sumtabs_cat$value, sumtabs_cat$name, print_cat),
-  file = here(output_dir, paste0(filenamebase, "_tabulate", ".txt")),
-  append=FALSE
-)
-
-
-### numeric ----
-sumtabs_num <-
-  data %>%
-  select(-ends_with("_id")) %>%
-  select(where(~ {!is.logical(.x) & is.numeric(.x) & !is.Date(.x)})) %>%
-  map(redacted_summary_num) %>%
-  enframe()
-
-capture.output(
-  walk2(sumtabs_num$value, sumtabs_num$name, print_num),
-  file = here(output_dir, paste0(filenamebase, "_tabulate", ".txt")),
-  append=TRUE
-)
-
-### dates ----
-
-sumtabs_date <-
-  data %>%
-  select(-ends_with("_id")) %>%
-  select(where(is.Date)) %>%
-  map(redacted_summary_date) %>%
-  enframe()
-
-capture.output(
-  walk2(sumtabs_date$value, sumtabs_date$name, print_num),
-  file = here(output_dir, paste0(filenamebase, "_tabulate", ".txt")),
-  append=TRUE
-)
